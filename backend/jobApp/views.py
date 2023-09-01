dcc3fd907a002ec32cab05b39600eaa724465fb9
Tim Meehan <meehanti23@gmail.com>
2023-09-01 14:47:35 -0400

    Backend API endpoint

--- backend/jobApp/views.py
+++ backend/jobApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import JobAppSerializer
+from .models import JobApp
 
 # Create your views here.
+
+class JobAppView(viewsets.ModelViewSet):
+    serializer_class = JobAppSerializer
+    queryset = JobApp.objects.all()
