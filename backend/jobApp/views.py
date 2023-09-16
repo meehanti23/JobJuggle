e3c86b8cf9b490a5d5740478e6df01b8ed7df3ec
Tim Meehan <meehanti23@gmail.com>
2023-09-16 15:03:09 -0400

    Initial Styling

--- backend/jobApp/views.py
+++ backend/jobApp/views.py
@@ -34,9 +34,16 @@ class JobCreateView(CreateView):
 class JobListView(ListView):
     model = JobApp
     context_object_name = "jobApps"
-    template_name = './home/jobList.html'
+    template_name = './home/job_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['applied_jobs_count'] = JobApp.objects.filter(status="Applied").count()
+        context['rejected_jobs_count'] = JobApp.objects.filter(status="Rejected").count()
+        context['interview_jobs_count'] = JobApp.objects.filter(status="Interview").count()
+        return context
 
 class JobDetailView(DetailView):
     model = JobApp
     context_object_name = "job"
-    template_name = './home/jobDetails.html'
+    template_name = './home/job_details.html'
